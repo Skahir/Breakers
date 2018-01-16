c8aafc0df2d058b519d85e31e6718677862c0d93
Nando Reij <33893915+Skahir@users.noreply.github.com>
2018-01-16 13:22:34 +0100

    wat dingen ge delet
    control alt delet

--- website/berekening.php
+++
@@ -1,5 +0,0 @@
-<?
-echo ord(substr(md5('password'),5,1)) * ord(substr(md5('password'),10,1)) * substr(md5('password'), 3, 1) - (ord('F')*100+ord('K'));
-?>
-
-<!-- Error executing SELECT name FROM thugs WHERE id = ../../../../../: You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near './../../../../' at line 1 -->
